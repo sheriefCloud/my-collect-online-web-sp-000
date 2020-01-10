ac2cedd1bc10321c72ebc3aee0936acddbec1ed1
Sherief Farid <sherief@farid.cloud>
2020-01-10 19:05:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,11 +8,6 @@ def my_collect(array)
     collection << yield(array[i])
     i +=1
   end
-  puts collection
   collection
 end
 
-array = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(array){|l| l.upcase}
-array = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
-my_collect(array){|n| n.split(" ").first}
