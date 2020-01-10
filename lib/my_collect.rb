198180f82f688919aa7aea2fc4403d5d53d553b3
Sherief Farid <sherief@farid.cloud>
2020-01-10 19:04:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -16,4 +16,3 @@ array = ['ruby', 'javascript', 'python', 'objective-c']
 my_collect(array){|l| l.upcase}
 array = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 my_collect(array){|n| n.split(" ").first}
-
